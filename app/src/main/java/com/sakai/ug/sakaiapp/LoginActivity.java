3f8941058d6877250dc0920a7717a862c8c06907
Degreat Yartey <yarteydegreat2@gmail.com>
2019-01-27 08:26:22 +0000

    Basic implementation for pages

---
+++ app/src/main/java/com/sakai/ug/sakaiapp/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.sakai.ug.sakaiapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/sakai/ug/sakaiapp/MainActivity.java
+++ app/src/main/java/com/sakai/ug/sakaiapp/MainActivity.java
@@ -1,13 +1,102 @@
 package com.sakai.ug.sakaiapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.sakai.ug.sakaiapp.fragments.HomeFragment;
+import com.sakai.ug.sakaiapp.fragments.NotificationFragment;
+import com.sakai.ug.sakaiapp.fragments.SiteFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private HomeFragment homeFragment = new HomeFragment();
+    private SiteFragment siteFragment = new SiteFragment();
+    private NotificationFragment notificationFragment = new NotificationFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ViewPager pager = findViewById(R.id.pager);
+
+        pager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                switch (i) {
+                    case 0:
+                        return homeFragment;
+                    case 1:
+                        return siteFragment;
+                    case 2:
+                        return notificationFragment;
+                    default:
+                        return new NotificationFragment(); // this is just a placeholder
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 4;
+            }
+        });
+
+        final BottomNavigationView bnNavigation = findViewById(R.id.bnNavigation);
+        bnNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        pager.setCurrentItem(0);
+                        break;
+                    case R.id.action_sites:
+                        pager.setCurrentItem(1);
+                        break;
+                    case R.id.action_notifications:
+                        pager.setCurrentItem(2);
+                        break;
+                    default:
+                        pager.setCurrentItem(3);
+                }
+
+                return true;
+            }
+        });
+
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                switch (i) {
+                    case 0:
+                        bnNavigation.setSelectedItemId(R.id.action_home);
+                        break;
+                    case 1:
+                        bnNavigation.setSelectedItemId(R.id.action_sites);
+                        break;
+                    case 2:
+                        bnNavigation.setSelectedItemId(R.id.action_notifications);
+                        break;
+
+                    default:
+                        bnNavigation.setSelectedItemId(R.id.action_chat);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
     }
 }
