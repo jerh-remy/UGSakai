70ad93a02773735faaa94a1fbb694b6877406863
namtay <ewoenamtay@gmail.com>
2019-03-04 15:08:29 +0000

    incomplete profile

--- app/src/main/java/com/sakai/ug/sakaiapp/ProfileActivity.java
+++ app/src/main/java/com/sakai/ug/sakaiapp/ProfileActivity.java
@@ -1,11 +1,14 @@
 package com.sakai.ug.sakaiapp;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class ProfileActivity extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
